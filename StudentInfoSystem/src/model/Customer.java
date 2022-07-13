49820c0979d552d30becf8caba6b7590a898b96e
Akash <Akash@DESKTOP-3MN3S9T>
2022-07-13 12:29:13 +0530

    customer.java

---
+++ StudentInfoSystem/src/model/Customer.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Customer {
+	int custId;
+
+	public int getCustId() {
+		return custId;
+	}
+
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+	
+
+}
