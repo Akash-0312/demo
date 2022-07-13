415abafa460ec2d2237e99ced6320bcbfd0328af
Akash <Akash@DESKTOP-3MN3S9T>
2022-07-13 13:10:03 +0530

    Customer updated

--- StudentInfoSystem/src/model/Customer.java
+++ StudentInfoSystem/src/model/Customer.java
@@ -2,6 +2,14 @@ package model;
 
 public class Customer {
 	int custId;
+    String customerName;
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
 
 	public int getCustId() {
 		return custId;
